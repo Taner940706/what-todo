c96b4b64a470fa549310295906d314c3ad792f1a
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-06-21 11:46:44 +0300

    add signup form

--- what-todo/src/pages/signup/Signup.js
+++ what-todo/src/pages/signup/Signup.js
@@ -1,9 +1,31 @@
+import {useState} from 'react'
 import styles from './Signup.module.css'
 
 export default function Signup(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        
+    }
+
     return (
-        <div>
-            Signup
-        </div>
+        <form onSubmit={handleSubmit} className={styles['sign-form']}>
+            <h2>Login</h2>
+            <label>
+                <span>Email:</span>
+                <input type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email} />
+            </label>
+            <label>
+                <span>Password:</span>
+                <input type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password} />
+            </label>
+            <button className='btn'>Signup</button>
+        </form>
     )
 }
