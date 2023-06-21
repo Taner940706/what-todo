5cd9fc9806a5aaea624717fef00cf320de010557
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-06-21 11:37:13 +0300

    create home page form

--- what-todo/src/pages/home/Home.js
+++ what-todo/src/pages/home/Home.js
@@ -1,9 +1,32 @@
+import {useState} from 'react'
 import styles from './Home.module.css'
 
 export default function Home(){
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        
+    }
+
     return (
-        <div>
-            Home
-        </div>
+        <form onSubmit={handleSubmit} className={styles['login-form']}>
+            <h2>Login</h2>
+            <label>
+                <span>Email:</span>
+                <input type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email} />
+            </label>
+            <label>
+                <span>Password:</span>
+                <input type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password} />
+            </label>
+            <button className='btn'>Login</button>
+        </form>
     )
 }
