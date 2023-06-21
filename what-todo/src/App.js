e630211f61552f263160cd2885e87247b6ce2e7e
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-06-21 11:16:44 +0300

    add navbar

--- what-todo/src/App.js
+++ what-todo/src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
      <BrowserRouter>
+     <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
