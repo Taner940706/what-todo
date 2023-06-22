83fc0c445b2b1c21c2ce4ce05bdca4822fa75df5
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-06-22 11:50:24 +0300

    logout hook

--- what-todo/src/components/Navbar.js
+++ what-todo/src/components/Navbar.js
@@ -1,14 +1,16 @@
 import {Link} from 'react-router-dom'
-
+import { useLogout } from '../hooks/useLogout'
 import styles from './Navbar.module.css'
 
 export default function Navbar(){
+    const {logout} = useLogout()
     return(
         <nav className={styles.navbar}>
             <ul>
                 <li className={styles.title}>WhatTodo</li>
                 <li><Link to="/login">Login</Link></li>
                 <li><Link to="/signup">Signup</Link></li>
+                <li onClick={logout}>Logout</li>
             </ul>
         </nav>
     )
